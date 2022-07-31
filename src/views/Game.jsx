f51c2736eae65e2d49c01fdc06547a2087a9fc33
Sito8943 <sito8943@gmail.com>
2022-07-31 15:02:40 -0700

    Snake done

--- src/views/Game.jsx
+++ src/views/Game.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import { useState, useCallback, useEffect } from "react";
+import SitoContainer from "sito-container";
 
 const Game = () => {
-  return <div></div>;
+  const [fieldSize, setFieldSize] = useState(16);
+  const [field, setField] = useState([]);
+
+  const [snake, setSnake] = useState([]);
+
+  const init = useCallback(() => {
+    const newField = [];
+    for (let i = 0; i < fieldSize; i += 1) newField.push(0);
+    setField(newField);
+    setSnake([
+      { y: fieldSize / 2, x: fieldSize / 2 },
+      { y: fieldSize / 2 - 1, x: fieldSize / 2 },
+    ]);
+  }, [fieldSize]);
+
+  useEffect(() => {
+    init();
+  }, [fieldSize, init]);
+
+  const cell = {
+    width: "30px",
+    height: "30px",
+    background: "#222333",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "1px",
+  };
+
+  // movement
+  const moveDown = () => {
+    
+  };
+
+  const moveUp = () => {};
+
+  const moveRight = () => {};
+
+  const moveLeft = () => {};
+
+  const isHead = (y, x) => snake[0].y === y && snake[0].x === x;
+
+  const isBody = (y, x) => {
+    const body = snake.filter((item, i) => {
+      if (item.y === y && item.x === x && i !== 0) return item;
+      return null;
+    });
+    console.log(body);
+    if (body.length) return true;
+    return false;
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {field.map((item, i) => (
+          <SitoContainer key={i}>
+            {field.map((jtem, j) => (
+              <SitoContainer key={j} sx={cell}>
+                {isHead(i, j) && <div className="snake-head" />}
+                {isBody(i, j) && <div className="snake-body" />}
+              </SitoContainer>
+            ))}
+          </SitoContainer>
+        ))}
+      </header>
+    </div>
+  );
 };
 
 export default Game;
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+// sito components
+import SitoContainer from "sito-container";
+
+// contexts
+import { useLanguage } from "../contexts/LanguageProvider";
+
+// images
+import logo from "../logo.svg";
 
 const NotFound = () => {
-  return <div></div>;
+  const { languageState } = useLanguage();
+
+  return (
+    <SitoContainer ignoreDefault className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{languageState.texts.NotFound.Title}</h1>
+        <p>{languageState.texts.NotFound.Description}</p>
+        <Link className="App-link" to="/game">
+          {languageState.texts.NotFound.Link}
+        </Link>
+      </header>
+    </SitoContainer>
+  );
 };
 
 export default NotFound;
